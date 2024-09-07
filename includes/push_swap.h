2ba6c13ee6fd158dfcbf88f77308b0ca807f209e
jsilva-m <jsilva-m@student.42malaga.com>
2024-09-07 11:31:18 +0200

    ajuste Makefile y push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -52,15 +52,24 @@ int			nbstr_cmp(const char *s1, const char *s2);
 2. ordena pilas pequeñas con un mínimo de movimientos
 3. ordenación principal */
 
-int			is_sorted(t_stack *stack);
+static int	find_highest_index(t_stack	*stack);
 void		tiny_sort(t_stack **stack);
+static void	push_all_save_three(t_stack	**stack_a, t_stack	**stack_b);
+static void	shift_stack(t_stack	**stack_a);
 void		sort(t_stack **stack_a, t_stack **stack_b);
+
+/*Main*/
+
+int			is_sorted(t_stack *stack);
+static void	push_swap(t_stack **stack_a, t_stack **stack_b, int stack_size)
 
 /* Position : 
 1. Obtiene el nodo con el índice más bajo
 2. calcula y asigna las posiciones objetivo para cada nodo en stack b */
 
+static void	get_position(t_stack	**stack);
 int			get_lowest_index_position(t_stack **stack);
+static int	get_target(t_stack	**a,  int	b_idx, int	target_idx, int	target_pos)
 void		get_target_position(t_stack **stack_a, t_stack **stack_b);
 
 /* Cost */
@@ -68,30 +77,41 @@ void		get_target_position(t_stack **stack_a, t_stack **stack_b);
 void		get_cost(t_stack **stack_a, t_stack **stack_b);
 void		do_cheapest_move(t_stack **stack_a, t_stack **stack_b);
 
-/* Calculate Move */
+/* Move operations */
 
 void		do_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
+void		do_rev_rotate_both(t_stack **a, t_stack **b, int *cost_a, int *cost_b);
+void		do_rotate_both(t_stack **a, t_stack **b, int *cost_a, int *cost_b);
+void		do_rotate_a(t_stack **a, int *cost_a);
+void		do_rotate_b(t_stack **b, int *cost_b);
 
 /* Operations */
 /* push */
 
+static void	push(t_stack	**src, t_stack	**dest);
 void		do_pa(t_stack **stack_a, t_stack **stack_b);
 void		do_pb(t_stack **stack_a, t_stack **stack_b);
 
 /*swap*/
 
+static void	swap(t_stack	*stack);
 void		do_sa(t_stack **stack_a);
 void		do_sb(t_stack **stack_b);
 void		do_ss(t_stack **stack_a, t_stack **stack_b);
 
+/*Reverse_rotate*/
+
+static void	rev_rotate(t_stack	**stack);
+void		do_rra(t_stack **stack_a);
+void		do_rrb(t_stack **stack_b);
+void		do_rrr(t_stack **stack_a, t_stack **stack_b);
+
 /*rotate*/
 
+static void	rotate(t_stack	*stack)
 void		do_ra(t_stack **stack_a);
 void		do_rb(t_stack **stack_b);
 void		do_rr(t_stack **stack_a, t_stack **stack_b);
-void		do_rrr(t_stack **stack_a, t_stack **stack_b);
-void		do_rra(t_stack **stack_a);
-void		do_rrb(t_stack **stack_b);
 
 /* Stack Functions
 1. obtiene el nodo infer */
@@ -99,7 +119,7 @@ void		do_rrb(t_stack **stack_b);
 t_stack		*get_stack_bottom(t_stack *stack);
 t_stack		*get_stack_before_bottom(t_stack *stack);
 t_stack		*stack_new(int value);
-void		stack_add_bottom(t_stack **stack, t_stack *new);
+void		stack_add_bottom(t_stack	**stack, t_stack	*new);
 int			get_stack_size(t_stack	*stack);
 
 /* Utils */
